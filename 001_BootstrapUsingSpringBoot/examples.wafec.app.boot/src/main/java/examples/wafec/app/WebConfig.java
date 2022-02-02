b24e30b4eab8787f895f4d710eeacd3ce2fc7a46
Wallace <wallacefcardoso@gmail.com>
2022-02-02 15:54:52 -0300

    spring boot 001

---
+++ 001_BootstrapUsingSpringBoot/examples.wafec.app.boot/src/main/java/examples/wafec/app/WebConfig.java
@@ -0,0 +1,12 @@
+package examples.wafec.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "examples.wafec.app.controller")
+public class WebConfig {
+
+}
