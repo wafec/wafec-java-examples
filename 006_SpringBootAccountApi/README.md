08db2dfe598b7c5e124858d97412e2d3a230496f
Wallace <wallacefcardoso@gmail.com>
2022-03-18 10:53:38 -0300

    add verify functionality

---
+++ 006_SpringBootAccountApi/README.md
@@ -0,0 +1,25 @@
+# How this app went to life
+
+## How to setup the basic rest application
+
+[https://spring.io/guides/gs/rest-service/](https://spring.io/guides/gs/rest-service/)
+
+## How to test a Spring rest application
+
+[https://spring.io/guides/gs/testing-web/](https://spring.io/guides/gs/testing-web/)
+
+## Find a Spring complete example
+
+[https://github.com/spring-guides/gs-rest-service/blob/main/complete/src/test/java/com/example/restservice/GreetingControllerTests.java]()
+
+## How to setup JWT
+
+[https://www.baeldung.com/java-json-web-tokens-jjwt]()
+
+[https://github.com/jwtk/jjwt]()
+
+P.S.: Baeldung is a little outdated, we do have to pass a key to sign the JWT.
+
+## How to generate the private key to Java
+
+[https://stackoverflow.com/questions/11410770/load-rsa-public-key-from-file]()
--- 006_SpringBootAccountApi/app/src/main/java/com/account/TokenController.java
+++ 006_SpringBootAccountApi/app/src/main/java/com/account/TokenController.java
@@ -3,12 +3,14 @@ package com.account;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Key;
+import java.security.KeyFactory;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.sql.Date;
 import java.time.Duration;
 import java.time.Instant;
@@ -16,9 +18,9 @@ import java.time.Instant;
 @RestController
 public class TokenController {
     @PostMapping("/token")
-    public String token(@RequestBody UserPassword userPassword) {
+    public String token(@RequestBody UserPassword userPassword) throws Exception {
         if (userPassword.getUser().equals("testuser1") && userPassword.getPassword().equals("Password123")) {
-            Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+            Key key = getKey();
             String jwt = Jwts.builder()
                     .setIssuer("Account")
                     .setSubject("account")
@@ -32,5 +34,40 @@ public class TokenController {
         } else {
             return ResponseEntity.badRequest().toString();
         }
+    }
+
+    private Key getKey() throws Exception {
+        byte[] keyBytes = TokenController.class.getResourceAsStream("/private_key.der").readAllBytes();
+        PKCS8EncodedKeySpec keySpec =
+                new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(keySpec);
+    }
+
+    @GetMapping("/token/verify")
+    public String verify(@RequestHeader HttpHeaders headers) throws Exception {
+        String authorization = headers.getFirst(HttpHeaders.AUTHORIZATION);
+        if (authorization.startsWith("Bearer")) {
+            String jws = authorization.substring("Bearer".length());
+            Key key = getPublicKey();
+            boolean valid = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jws)
+                    .getBody()
+                    .getSubject()
+                    .equals("account");
+            return valid ? "JWT is valid" : "JWT is not valid";
+        } else {
+            return ResponseEntity.badRequest().toString();
+        }
+    }
+
+    private Key getPublicKey() throws Exception {
+        byte[] keyBytes = TokenController.class.getResourceAsStream("/public_key.der").readAllBytes();
+        X509EncodedKeySpec keySpec =
+                new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(keySpec);
     }
 }
